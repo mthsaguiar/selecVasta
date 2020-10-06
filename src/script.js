e8b6a4926ca0d241d66e7c64560d78bc9d8afee3
mthsaguiar <matheusaguiardev@gmail.com>
2020-10-06 15:06:42 -0300

    mobile layout 80%

--- src/script.js
+++ src/script.js
@@ -1,3 +1,12 @@
 document.querySelector("form").addEventListener("submit", e=>{
     e.preventDefault();
 })
+
+function closeFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
